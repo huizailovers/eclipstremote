576912b6404cc8605b26b777d06a36eafbd1a984
huizailovers <596945658@qq.com>
2017-07-31 14:45:10 +0800

    dev第三次提交

--- active-demo/src/com/kd/activemq/demo2.java
+++ active-demo/src/com/kd/activemq/demo2.java
@@ -4,6 +4,8 @@ public class demo2 {
 	public static void main(String[] args) {
 		System.err.println("demo测试");
 		System.err.println("dev 分支第二次提交");
+		System.err.println("dev 分支的第三次提交");
+		
 	}
 
 }
